f3a06605920142051f77fbb1a4904c1e48054fa9
Petermuraya <134588157+Petermuraya@users.noreply.github.com>
2024-09-29 19:18:56 +0000

    delete

--- karatina-red-cross/vite.config.ts
+++ karatina-red-cross/vite.config.ts
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base:"/codespace-didactic-giggle-vx7x4wvwrqgcw4qj/",
 })
